d758368349c00202ef2566316472fa20386e81db
Kari Adilova <kariadilovaadilova7@gmail.com>
2023-02-16 18:16:58 +0500

    feat: implement Morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = [];
+    let result = '';
+    for (let i = 0; i < expr.length; i = i + 10) {
+        array.push(expr.substring(i, i + 10));
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === '**********') {
+            result = result + ' '; 
+        } else {
+            let letter = '';
+            for (let j = 0; j < 10; j++) {
+                if (array[i][j] === '1') {
+                    letter = array[i].substring(j);
+                    break;
+                }
+            }
+            let letterCode = [];
+            for (let j = 0; j < letter.length; j = j + 2) {
+                letterCode.push(letter.substring(j, j + 2));
+            }
+            let letterCodeMorse = '';
+            for (let j = 0; j < letterCode.length; j++) {
+                if (letterCode[j] === '10') {
+                   letterCodeMorse = letterCodeMorse + '.';
+                } else {
+                    letterCodeMorse = letterCodeMorse + '-';
+                }
+            }
+            result = result + MORSE_TABLE[letterCodeMorse];            
+        }
+    }
+    return result;
 }
 
 module.exports = {
